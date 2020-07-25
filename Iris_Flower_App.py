0fc67a5f45620b762f41dcb9a7940fad6e1ef6e1
Rekidiang2 <rkiese6@gmail.com>
2020-07-26 00:49:25 +0800

    iris first1

---
+++ Iris_Flower_App.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    """Iris Flower EDA application"""
+
+    st.title("Iris Flower EDA application")
+    st.subheader("Streamlit is cool")
+
+if if __name__ == "__main__":
+    main()
